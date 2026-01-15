02b8a1d2c95301d750a3dedf6f8df823287629b0
Lina Derbal <linaderbal999@gmail.com>
2026-01-15 22:50:43 +0100

    Add hexagon drawing functionality with colors

---
+++ turtle lina.py
@@ -0,0 +1,17 @@
+import turtle
+sideLength = int(input("Enter the size of each side: "))
+screen = turtle.Screen()
+screen.clearscreen()
+hexagon = turtle.Turtle()
+colors = ["#7209B7", "#FEE440","#FF37A6", "#A7C957", "#4CC9F0", "#D67316"]
+hexagon.speed(1)
+hexagon.fillcolor("#FFCAE9")
+hexagon.pensize(2)
+hexagon.begin_fill()
+for i in range(6):
+    hexagon.pencolor(colors[i])
+    hexagon.forward(sideLength)
+    hexagon.right(60)
+
+hexagon.end_fill()
+turtle.done()
